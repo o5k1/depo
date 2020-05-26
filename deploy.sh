e157a4869c1f7bff95a4dedb1005e0720a5f554b
Andrea Tombolato <tombolato.andrea@gmail.com>
2020-05-26 09:05:22 +0200

    CHANGE put sentry auth token in local .env file

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# import var from .env file
+cat .env
+
 # show MESSAGE while running COMMAND, COMMAND stdout is hidden
 run_command() {
   COMMAND="$1"
@@ -40,7 +43,6 @@ export GIT_MERGE_AUTOEDIT=no
 VERSION="v$1"
 SENTRY_VERSION="depo@$VERSION"
 
-export SENTRY_AUTH_TOKEN=11db4445f6264ad38962708257aeefd5d50d82576c884765a6f9a312c799afff
 export SENTRY_ORG=reffable
 export SENTRY_PROJECT=depo
 
